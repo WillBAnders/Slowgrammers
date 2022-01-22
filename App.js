071848e7cad951c691012b96cea0dff0a9a5a2a5
cole-kitroser <70605063+cole-kitroser@users.noreply.github.com>
2022-01-22 11:20:51 -0500

    Add files via upload
    The buttons are now working as intended, with a button pulling up a window to show the available tutors, which needs to be programmed in. I added some comments in to help show what goes where.

--- App.js
+++ App.js
@@ -12,47 +12,27 @@ import { useTheme } from "@mui/material/styles";
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/material/Icon";
 
-function OpenTutorWindow() {
-  const [open, setOpen] = React.useState(true);
+function App(classes) {
+  {/* Needed to open and close the tutor window */}
+  const [open, setOpen] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
-  /*function handleClickOpen() {
-    setOpen(true);
-    console.log("open");
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const [title, setTitle] = React.useState("");
+  function SetTitle(classname){
+    console.log(classname);
+    setTitle(classname);
+    console.log(title);
   }
-
-  function handleClose() {
+  const handleClickOpen = (classname) => {
+    setOpen(true);
+    SetTitle(classname);
+  };
+  const handleClose = () => {
     setOpen(false);
-    console.log("close");
-  }*/
-  return (
-    <Dialog
-      fullScreen={fullScreen}
-      open={open}
-      onClose={setOpen(false)}
-      aria-labelledby="Tutor Window"
-    >
-      <DialogTitle id="Tutor Window">
-        {"Pick your tutor for this class"}
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          If you're seeing this, that means that the dialog box is working.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <IconButton aria-label="close">
-          <CloseIcon />
-        </IconButton>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
-function App() {
-  var otw = OpenTutorWindow;
+  };
   return (
     <div className="Classes">
+      {/* Class Buttons */}
       <Stack direction="column" spacing={{ xs: 1, sm: 2, md: 2.5 }}>
         <Stack
           direction={{ xs: "column", sm: "row" }}
@@ -60,30 +40,22 @@ function App() {
           justifyContent="center"
           alignItems="center"
         >
-          {/*onClick={() => {
-    alert('clicked');
-  }} */}
           <Button
             variant="contained"
-            onClick={() => {
-              otw();
-            }}
+            onClick={() => handleClickOpen("COP3502C")}
           >
             COP3502C-Programming Fundamentals 1
+            {console.log(title)}
           </Button>
           <Button
             variant="contained"
-            onClick={() => {
-              console.clear();
-            }}
+            onClick={() => handleClickOpen("COP3503C")}
           >
             COP3503C-Programming Fundamentals 2
           </Button>
           <Button
             variant="contained"
-            onClick={() => {
-              console.log("test");
-            }}
+            onClick={() => handleClickOpen("COP3530")}
           >
             COP3530- Data Structures and Algorithms
           </Button>
@@ -94,13 +66,22 @@ function App() {
           justifyContent="center"
           alignItems="center"
         >
-          <Button variant="contained">
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("COT3100")}
+          >
             COT3100- Applications of Discrete Structures
           </Button>
-          <Button variant="contained">
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("CDA3101")}
+          >
             CDA3101- Introduction to Computer Organization
           </Button>
-          <Button variant="contained">
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("CEN3031")}
+          >
             CEN3031- Introduction to Software Engineering
           </Button>
         </Stack>
@@ -110,14 +91,23 @@ function App() {
           justifyContent="center"
           alignItems="center"
         >
-          <Button variant="contained">
-            CIS4301- Information and Database Systems"
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("CIS4301")}
+          >
+            CIS4301- Information and Database Systems
           </Button>
-          <Button variant="contained">
-            CNT4007- Computer Network Fundamentals"
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("CNT4007")}
+          >
+            CNT4007- Computer Network Fundamentals
           </Button>
-          <Button variant="contained">
-            COP4020- Programming Language Concepts"
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("COP4020")}
+          >
+            COP4020- Programming Language Concepts
           </Button>
         </Stack>
         <Stack
@@ -126,12 +116,41 @@ function App() {
           justifyContent="center"
           alignItems="center"
         >
-          <Button variant="contained">
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("COP4533")}
+          >
             COP4533- Algorithm Abstraction and Design
           </Button>
-          <Button variant="contained">COP4600- Operating Systems</Button>
+          <Button
+            variant="contained"
+            onClick={() => handleClickOpen("COP4600")}
+          >
+            COP4600- Operating Systems
+          </Button>
         </Stack>
       </Stack>
+      {/* Window Part */}
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="Tutor Window"
+      >
+        <DialogTitle id="Tutor Window">
+          {"Pick your tutor for " + title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            If you're seeing this, that means that the dialog box is working.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <IconButton aria-label="close">
+            <CloseIcon />
+          </IconButton>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
