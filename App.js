01699dd8840ae5ba582855a1bc7ea2d7ccf7ab54
cole-kitroser <colekitroser@ufl.edu>
2022-01-24 13:04:05 -0500

    feat- buttons truncated to single stack

--- App.js
+++ App.js
@@ -12,123 +12,52 @@ import { useTheme } from "@mui/material/styles";
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/material/Icon";
 
-function App(classes) {
-  {/* Needed to open and close the tutor window */}
+
+
+function App() {
+let courses = []
+courses.push("COP3502C- Programming Fundamentals 1");
+courses.push("COP3503C- Programming Fundamentals 2");
+courses.push("COP3530- Data Structures and Algorithms");
+courses.push("COT3100- Applications of Discrete Structures");
+courses.push("CDA3101- Introduction to Computer Organization");
+courses.push("CEN3031- Introduction to Software Engineering");
+courses.push("CIS4301- Information and Database Systems");
+courses.push("CNT4007- Computer Network Fundamentals");
+courses.push("COP4020- Programming Language Concepts");
+courses.push("COP4533- Algorithm Abstraction and Design");
+courses.push("COP4600- Operating Systems");
+  // Needed to open and close the tutor window
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
   const [title, setTitle] = React.useState("");
-  function SetTitle(classname){
-    console.log(classname);
-    setTitle(classname);
-    console.log(title);
-  }
-  const handleClickOpen = (classname) => {
+  const handleClickOpen = (coursename) => {
     setOpen(true);
-    SetTitle(classname);
+    setTitle(coursename);
   };
   const handleClose = () => {
     setOpen(false);
   };
+  // Used for setting up the importing of course names and turning them into buttons
+  let arrButtons = [];
+  for (let i = 0; i < courses.length; i++){
+    let text = courses.at(i);
+    arrButtons.push(
+      <Button 
+        variant="contained" 
+        text= {courses.at(i)}
+        onClick={() => handleClickOpen(text)}
+      >
+        {text}
+      </Button>
+    );
+  }
   return (
-    <div className="Classes">
-      {/* Class Buttons */}
+    <div className="Courses">
+      {/* Course Buttons */}
       <Stack direction="column" spacing={{ xs: 1, sm: 2, md: 2.5 }}>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP3502C")}
-          >
-            COP3502C-Programming Fundamentals 1
-            {console.log(title)}
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP3503C")}
-          >
-            COP3503C-Programming Fundamentals 2
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP3530")}
-          >
-            COP3530- Data Structures and Algorithms
-          </Button>
-        </Stack>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COT3100")}
-          >
-            COT3100- Applications of Discrete Structures
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("CDA3101")}
-          >
-            CDA3101- Introduction to Computer Organization
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("CEN3031")}
-          >
-            CEN3031- Introduction to Software Engineering
-          </Button>
-        </Stack>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("CIS4301")}
-          >
-            CIS4301- Information and Database Systems
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("CNT4007")}
-          >
-            CNT4007- Computer Network Fundamentals
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP4020")}
-          >
-            COP4020- Programming Language Concepts
-          </Button>
-        </Stack>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP4533")}
-          >
-            COP4533- Algorithm Abstraction and Design
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => handleClickOpen("COP4600")}
-          >
-            COP4600- Operating Systems
-          </Button>
-        </Stack>
+        {arrButtons} 
       </Stack>
       {/* Window Part */}
       <Dialog
