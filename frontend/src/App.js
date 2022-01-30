ccbf9e39574aa45a938baaff5860f145ca5b1be3
chris-brugal <chrisbrugal@yahoo.com>
2022-01-29 21:18:49 -0500

    feat: added sign in and sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import CoursePage from "./components/CoursePage"
 import LandingPage from "./components/LandingPage"
 import TutorPage from './components/TutorPage'
 import SignUpPage from "./components/SignUpPage";
+import SignInPage from "./components/SigninPage";
 import Navbar from './components/Navbar';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/Courses" element={<CoursePage/>}/>
           <Route path="/Tutors" element={<TutorPage />} />
           <Route path="/SignUp" element={<SignUpPage/>} />
+          <Route path="/SignIn" element={<SignInPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
