17158148805bf6f7855bfb577d601fd13f703e31
WillBAnders <WillBAnders@gmail.com>
2022-02-10 10:00:46 -0500

    Add backend testing for individual course/tutor routes
    This fixes two implementation issues; order was not being applied
    (mainly due to earlier issues with Gorm) and the result slice was not
    initialized thus returning empty (Gorm only assigns if results are
    present).

--- backend/src/router_test.go
+++ backend/src/router_test.go
@@ -85,6 +85,78 @@ func (suite *RouterSuite) TestGetCourses() {
 	))
 }
 
+func (suite *RouterSuite) TestGetCoursesCode() {
+	//Fix ID to ensure multiple references use the same course
+	course := Course{ID: 1, Code: "code", Name: "Name"}
+
+	test := func(tutorings []Tutoring, expected string) func() {
+		return manualSetupTest(func() {
+			DB.Create(&course)
+			DB.Create(tutorings)
+			w := request("GET", "/courses/"+course.Code)
+			suite.Equal(200, w.Code)
+			suite.JSONEq(expected, w.Body.String())
+		})
+	}
+
+	suite.Run("Empty Tutors", test(
+		[]Tutoring{},
+		`{
+			"course": {"code": "code", "name": "Name"},
+			"tutors": []
+		}`,
+	))
+
+	suite.Run("Single Tutor", test(
+		[]Tutoring{
+			{Course: course, Tutor: Tutor{Username: "Username"}},
+		},
+		`{
+			"course": {"code": "code", "name": "Name"},
+			"tutors": [
+				{"username": "Username"}
+			]
+		}`,
+	))
+
+	suite.Run("Multiple Tutors", test(
+		[]Tutoring{
+			{Course: course, Tutor: Tutor{Username: "Alice"}},
+			{Course: course, Tutor: Tutor{Username: "Bob"}},
+			{Course: course, Tutor: Tutor{Username: "Clair"}},
+		},
+		`{
+			"course": {"code": "code", "name": "Name"},
+			"tutors": [
+				{"username": "Alice"},
+				{"username": "Bob"},
+				{"username": "Clair"}
+			]
+		}`,
+	))
+
+	suite.Run("Tutors Order", test(
+		[]Tutoring{
+			{Course: course, Tutor: Tutor{Username: "Clair"}},
+			{Course: course, Tutor: Tutor{Username: "Bob"}},
+			{Course: course, Tutor: Tutor{Username: "Alice"}},
+		},
+		`{
+			"course": {"code": "code", "name": "Name"},
+			"tutors": [
+				{"username": "Alice"},
+				{"username": "Bob"},
+				{"username": "Clair"}
+			]
+		}`,
+	))
+
+	suite.Run("Undefined Code", manualSetupTest(func() {
+		w := request("GET", "/courses/undefined")
+		suite.Equal(404, w.Code)
+	}))
+}
+
 func (suite *RouterSuite) TestGetTutors() {
 	test := func(tutors []Tutor, expected string) func() {
 		return manualSetupTest(func() {
@@ -142,6 +214,78 @@ func (suite *RouterSuite) TestGetTutors() {
 			]
 		}`,
 	))
+}
+
+func (suite *RouterSuite) TestGetTutorsUsername() {
+	//Fix ID to ensure multiple references use the same tutor
+	tutor := Tutor{ID: 1, Username: "Username"}
+
+	test := func(tutorings []Tutoring, expected string) func() {
+		return manualSetupTest(func() {
+			DB.Create(&tutor)
+			DB.Create(tutorings)
+			w := request("GET", "/tutors/"+tutor.Username)
+			suite.Equal(200, w.Code)
+			suite.JSONEq(expected, w.Body.String())
+		})
+	}
+
+	suite.Run("Empty Courses", test(
+		[]Tutoring{},
+		`{
+			"tutor": {"username": "Username"},
+			"courses": []
+		}`,
+	))
+
+	suite.Run("Single Tutor", test(
+		[]Tutoring{
+			{Course: Course{Code: "code", Name: "Name"}, Tutor: tutor},
+		},
+		`{
+			"tutor": {"username": "Username"},
+			"courses": [
+				{"code": "code", "name": "Name"}
+			]
+		}`,
+	))
+
+	suite.Run("Multiple Tutors", test(
+		[]Tutoring{
+			{Course: Course{Code: "1", Name: "First"}, Tutor: tutor},
+			{Course: Course{Code: "2", Name: "Second"}, Tutor: tutor},
+			{Course: Course{Code: "3", Name: "Third"}, Tutor: tutor},
+		},
+		`{
+			"tutor": {"username": "Username"},
+			"courses": [
+				{"code": "1", "name": "First"},
+				{"code": "2", "name": "Second"},
+				{"code": "3", "name": "Third"}
+			]
+		}`,
+	))
+
+	suite.Run("Courses Order", test(
+		[]Tutoring{
+			{Course: Course{Code: "3", Name: "Third"}, Tutor: tutor},
+			{Course: Course{Code: "2", Name: "Second"}, Tutor: tutor},
+			{Course: Course{Code: "1", Name: "First"}, Tutor: tutor},
+		},
+		`{
+			"tutor": {"username": "Username"},
+			"courses": [
+				{"code": "1", "name": "First"},
+				{"code": "2", "name": "Second"},
+				{"code": "3", "name": "Third"}
+			]
+		}`,
+	))
+
+	suite.Run("Undefined Username", manualSetupTest(func() {
+		w := request("GET", "/tutors/undefined")
+		suite.Equal(404, w.Code)
+	}))
 }
 
 func request(method string, path string) *httptest.ResponseRecorder {
