62f4070bd330315028a548e34cc8b730e63cb7d8
WillBAnders <WillBAnders@gmail.com>
2022-03-25 23:41:01 -0400

    Refactor CoursesPage and add test coverage
    The core behavior is the same, but this cleans up some of the logic and
    adds tests. Error handling for fetch responses still needs to be done,
    but this is probably better to do at the project level.

---
+++ frontend/src/tests/components/CoursesPage.test.js
@@ -0,0 +1,88 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import "regenerator-runtime/runtime";
+import "@testing-library/jest-dom";
+import {MemoryRouter} from "react-router-dom";
+import {render, fireEvent, waitFor} from "@testing-library/react";
+import CoursesPage from "../../components/CoursesPage.js";
+
+beforeAll(() => {
+  global.fetch = jest.fn();
+  global.fetch.mockResponseValue = function (value) {
+    this.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(value)
+    });
+  };
+});
+
+describe("CoursesPage", () => {
+  test("loading", async () => {
+    fetch.mockResponseValue({courses: [{code: "code", name: "Name"}]});
+    await waitFor(async () => {
+      const component = render(<CoursesPage/>, {wrapper: MemoryRouter});
+      const cards = component.queryByTestId(/buttonStack/i);
+      expect(cards.children.length).toBe(0);
+    })
+  });
+
+  test("fetch route", async () => {
+    fetch.mockResponseValue({courses: []});
+    const component = await waitFor(async () => {
+      return render(<CoursesPage/>, {wrapper: MemoryRouter});
+    });
+    expect(fetch).toHaveBeenCalledWith("/courses");
+  });
+
+  describe("courses", () => {
+    test.each`
+      name          | courses
+      ${"empty"}    | ${[]}
+      ${"single"}   | ${[{code: "code", name: "Name"}]}
+      ${"multiple"} | ${[{code: "code-1", name: "First"}, {code: "code-2", name: "Second"}, {code: "code-3", name: "Third"}]}
+    `("$name", async ({courses}) => {
+      fetch.mockResponseValue({courses});
+      const component = await waitFor(async () => {
+        return render(<CoursesPage/>, {wrapper: MemoryRouter});
+      });
+      const cards = component.queryByTestId(/buttonStack/i);
+      expect(cards.children.length).toBe(courses.length);
+    });
+  });
+
+  describe("filter", () => {
+    test.each`
+      name                | filter         | matched
+      ${"code lowercase"} | ${"code"}      | ${true}
+      ${"code uppercase"} | ${"CODE"}      | ${true}
+      ${"name lowercase"} | ${"name"}      | ${true}
+      ${"name uppercase"} | ${"NAME"}      | ${true}
+      ${"partial"}        | ${"e"}         | ${true}
+      ${"unmatched"}      | ${"unmatched"} | ${false}
+    `("$name", async ({filter, matched}) => {
+      fetch.mockResponseValue({courses: [{code: "code", name: "Name"}]});
+      const component = await waitFor(async () => {
+        return render(<CoursesPage/>, {wrapper: MemoryRouter});
+      });
+      await waitFor(() => {
+        const search = component.queryByTestId(/SearchBarin/i);
+        fireEvent.change(search, {target: {value: filter}});
+      });
+      const cards = component.queryByTestId(/buttonStack/i);
+      expect(cards.children.length).toBe(matched ? 1 : 0);
+    });
+  });
+
+  test("fetch rejected", async () => {
+    console.error = jest.fn(); //TODO: State management
+    fetch.mockRejectedValue(new Error("expected"));
+    const component = await waitFor(async () => {
+      return render(<CoursesPage/>, {wrapper: MemoryRouter});
+    });
+    const cards = component.queryByTestId(/buttonStack/i);
+    expect(cards.children.length).toBe(0);
+    expect(console.error).toHaveBeenCalledWith("expected");
+  });
+});
