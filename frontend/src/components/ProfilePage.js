8d511b29b7a46f718e3162bb90aa1f83b439a1dd
cole-kitroser <colekitroser@ufl.edu>
2022-04-04 14:06:09 -0400

    Added the ability to pick availabilities
    Hypothetical code for showing availability
    
    I have made some progress, but I can't get the availability cards to show on the page
    
    It's working... somewhat
    
    Availability is now not causing infinite rerenders and is showing more than one object, but is still bugged
    
    Adding times works at a functional level
    
    Times will not overlap
    
    Remove time is semi-functional
    
    Deleting and sorting are functioning
    
    Adding availability has been refined, merging mostly works
    
    I immediately found an unrelated but still relevant bug and squashed it
    
    Another bug
    
    Just making sure all of my work is on github
    
    Just making sure all of my work is on github
    
    I had to do a manual rebase because it was messing up the work I've done something fierce
    
    Revert "Just making sure all of my work is on github"
    
    This reverts commit 36339723d29b2eb6adf8fed9ccc406d3759b2a9a.
    
    Logic is correct, but unless more than 2 times are merging, the website doesn't visually update until something else happens.
    
    Merging of more than two times wasn't working temporarily
    
    Pushing what I have so I can go ahead and work on getting addcourse to work properly
    
    Availability selector is functioning

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -4,11 +4,22 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import { ThreeDots } from "react-loader-spinner";
+import Autocomplete from "@mui/material/Autocomplete";
+import { days, times } from "../styles/TableData";
 import Utils from "../Utils";
 
 export default function ProfilePage({ profile }) {
   const [updated, setUpdated] = React.useState({});
+  const [day, setDay] = React.useState("");
+  const [startTime, setStartTime] = React.useState("");
+  const [endTime, setEndTime] = React.useState("");
+  const [availability, setAvailability] = React.useState([]);
 
   function onSubmit(event) {
     event.preventDefault();
@@ -26,6 +37,231 @@ export default function ProfilePage({ profile }) {
 
   function update(field, value) {
     setUpdated((fields) => ({ ...fields, [field]: value }));
+  }
+
+  const checkTimeValidity = () => {
+    const start = times.indexOf(startTime);
+    const end = times.indexOf(endTime);
+    return availability
+      .filter((a) => day === a.day)
+      .every(
+        (a) =>
+          start > times.indexOf(a.endTime) || end < times.indexOf(a.startTime)
+      );
+  };
+
+  const addTime = (event) => {
+    event.preventDefault();
+    if (!day || !startTime || !endTime) {
+      alert("Incomplete time inputed");
+      return;
+    }
+    const stIndex = times.indexOf(startTime);
+    const etIndex = times.indexOf(endTime);
+    if (
+      etIndex <= stIndex ||
+      availability.some(
+        (a) =>
+          day === a.day &&
+          stIndex < times.indexOf(a.endTime) &&
+          etIndex > times.indexOf(a.startTime)
+      )
+    ) {
+      alert("Invalid time inputed");
+      return;
+    }
+    const index = availability.findIndex(
+      (a) =>
+        day === a.day &&
+        (stIndex === times.indexOf(a.endTime) ||
+          etIndex === times.indexOf(a.startTime))
+    );
+    const temp = [...availability];
+    if (index !== -1) {
+      if (
+        index + 1 < temp.length &&
+        temp[index + 1].day === day &&
+        times.indexOf(temp[index + 1].startTime) === etIndex
+      ) {
+        temp.splice(index, 2, {
+          day,
+          startTime:
+            times[Math.min(stIndex, times.indexOf(temp[index].startTime))],
+          endTime:
+            times[Math.max(etIndex, times.indexOf(temp[index + 1].endTime))],
+        });
+      } else {
+        temp[index] = {
+          day,
+          startTime:
+            times[
+              Math.min(stIndex, times.indexOf(availability[index].startTime))
+            ],
+          endTime:
+            times[
+              Math.max(etIndex, times.indexOf(availability[index].endTime))
+            ],
+        };
+      }
+    } else {
+      temp.push({ day, startTime, endTime });
+      temp.sort((a, b) => {
+        return a.day === b.day
+          ? times.indexOf(a.startTime) - times.indexOf(b.startTime)
+          : days.indexOf(a.day) - days.indexOf(b.day);
+      });
+    }
+    setAvailability(temp);
+    // [12am-3am, 7am-9am]
+    // Case 1: [6am-8am] (good)
+    // Case 2: [3am-7am] (not done)
+    // Case 3: [7am-10am] (good)
+
+    //const astIndex = times.indexOf(availability[index].startTime);
+    //const aetIndex = times.indexOf(availability[index].endTime);
+    /*
+    if (sstIndex >= astIndex) {
+        availability[index].endTime = endTime;
+    } else if (etIndex <= aetIndex) {
+        availability[index].startTime = startTime;
+    } else {
+        availability.splice()
+    }
+    */
+  };
+
+  const merge = () => {
+    let stIndex = times.indexOf(startTime);
+    let etIndex = times.indexOf(endTime);
+    let st = startTime;
+    let et = endTime;
+    let mergecount = 0;
+    for (let i = 0; i < availability.length; i++) {
+      if (day === availability[i].day) {
+        const avsIndex = times.indexOf(availability[i].startTime);
+        const aveIndex = times.indexOf(availability[i].endTime);
+        if (stIndex < avsIndex && etIndex < aveIndex) {
+          console.log("Changing start");
+          availability[i].startTime = st;
+          et = availability[i].endTime;
+          if (mergecount > 0) {
+            const temparray = [...availability];
+            temparray.splice(i - 1, 1);
+            setAvailability(temparray);
+          }
+          mergecount++;
+          stIndex = times.indexOf(st);
+          etIndex = times.indexOf(et);
+        } else if (stIndex > avsIndex && etIndex > aveIndex) {
+          console.log("Changing end");
+          availability[i].endTime = et;
+          st = availability[i].startTime;
+          if (mergecount > 0) {
+            const temparray = [...availability];
+            temparray.splice(i, 1);
+            setAvailability(temparray);
+          }
+          mergecount++;
+          stIndex = times.indexOf(st);
+          etIndex = times.indexOf(et);
+        } else if (
+          stIndex <= avsIndex &&
+          etIndex >= aveIndex &&
+          !(stIndex === avsIndex && etIndex === aveIndex)
+        ) {
+          availability[i].startTime = st;
+          availability[i].endTime = et;
+          st = availability[i].startTime;
+          et = availability[i].endTime;
+          if (mergecount > 0) {
+            const temparray = [...availability];
+            temparray.splice(i, 1);
+            setAvailability(temparray);
+          }
+          mergecount++;
+          stIndex = times.indexOf(st);
+          etIndex = times.indexOf(et);
+        }
+      }
+    }
+    return mergecount > 0;
+  };
+
+  const addTimeOld = (event) => {
+    event.preventDefault();
+    console.log(day + ", " + startTime + "-" + endTime);
+    if (!day || !startTime || !endTime) {
+      console.log("Invalid time");
+      alert("Incomplete entry made");
+    } else if (
+      times.indexOf(startTime) < times.indexOf(endTime) &&
+      checkTimeValidity()
+    ) {
+      console.log("Valid time");
+      const newday = { day: day, startTime: startTime, endTime: endTime };
+      setAvailability((availability) => {
+        return [...availability, newday].sort((a, b) => {
+          return a.day === b.day
+            ? times.indexOf(a.startTime) - times.indexOf(b.startTime)
+            : days.indexOf(a.day) - days.indexOf(b.day);
+        });
+      });
+      setDay("");
+      setStartTime("");
+      setEndTime("");
+    } else {
+      if (!merge()) {
+        console.log("Invalid time");
+        alert("Invalid time chosen");
+      } else {
+        setDay("");
+        setStartTime("");
+        setEndTime("");
+      }
+    }
+  };
+
+  const removeTime = (event, index) => {
+    event.preventDefault();
+    const temparray = [...availability];
+    temparray.splice(index, 1);
+    setAvailability(temparray);
+  };
+
+  function showAvailability(availabil) {
+    let availcards = [];
+    console.log("Availability", availabil);
+    if (availabil.length != 0) {
+      for (let i = 0; i < availabil.length; i++) {
+        availcards.push(
+          <Stack
+            direction="row"
+            key={i}
+            component="form"
+            onSubmit={(e) => removeTime(e, i)}
+          >
+            <Card>
+              <CardContent>
+                {availabil[i].day}: {availabil[i].startTime} -{" "}
+                {availabil[i].endTime}
+              </CardContent>
+            </Card>
+            <Button
+              aria-label="DeleteIcon"
+              variant="contained"
+              color="error"
+              type="submit"
+              title="removebutton"
+            >
+              <DeleteIcon />
+            </Button>
+          </Stack>
+        );
+      }
+      return availcards;
+    } else {
+      return <div>No availability given</div>;
+    }
   }
 
   if (profile === null) {
@@ -106,10 +342,90 @@ export default function ProfilePage({ profile }) {
                 />
               </Grid>
             </Grid>
+            <TextField
+              required
+              fullWidth
+              title="bio"
+              id="bio"
+              label="Biography"
+              defaultValue={profile.bio ?? ""}
+              onChange={(e) => update("bio", e.target.value)}
+              multiline
+              rows={5}
+              maxRows={8}
+              sx={{ m: 1, pl: 1 }}
+            />
             <Button type="submit" title="submit" variant="contained">
               Update
             </Button>
           </Box>
+        </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ mt: 2 }}
+          component="form"
+          onSubmit={addTime}
+          noValidate
+        >
+          <Stack direction="row">
+            <Autocomplete
+              disablePortal
+              id="DaySelector"
+              value={day}
+              onChange={(event, newValue) => {
+                setDay(newValue);
+              }}
+              options={days}
+              sx={{ width: { xs: 100, md: 300 } }}
+              renderInput={(params) => <TextField {...params} label="Day" />}
+            />
+            <Autocomplete
+              disablePortal
+              id="StartTimeSelector"
+              options={times}
+              value={startTime}
+              onChange={(event, newValue) => {
+                setStartTime(newValue);
+              }}
+              sx={{ width: { xs: 100, md: 300 } }}
+              renderInput={(params) => (
+                <TextField {...params} label="Start Time" />
+              )}
+            />
+            <Autocomplete
+              disablePortal
+              id="EndTimeSelector"
+              value={endTime}
+              onChange={(event, newValue) => {
+                setEndTime(newValue);
+              }}
+              options={times}
+              sx={{ width: { xs: 100, md: 300 } }}
+              renderInput={(params) => (
+                <TextField {...params} label="End Time" />
+              )}
+            />
+
+            <Button
+              type="submit"
+              aria-label="AddIcon"
+              variant="contained"
+              color="success"
+              title="addbutton"
+            >
+              <AddIcon />
+            </Button>
+          </Stack>
+        </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ mt: 2 }}
+        >
+          <Stack>{showAvailability(availability)}</Stack>
         </Box>
       </div>
     );
