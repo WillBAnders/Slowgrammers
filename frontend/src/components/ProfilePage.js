96e2f94d531958917c73644d1791f90efa82ed47
cole-kitroser <colekitroser@ufl.edu>
2022-04-04 14:06:09 -0400

    Added the ability to pick availabilities

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -1,11 +1,15 @@
-import { React, useState, useEffect, Suspense } from "react";
+import React from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { Navigate } from 'react-router';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack'
 import { ThreeDots } from 'react-loader-spinner';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
 const ProfilePage = (user) => {
@@ -14,6 +18,9 @@ const ProfilePage = (user) => {
     const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+    const [day, setDay] = React.useState('');
+    const [startTime, setStartTime] = React.useState('');
+    const [endTime, setEndTime] = React.useState('');
 
     useEffect(() => {
         if (user.user != null) {
@@ -25,6 +32,144 @@ const ProfilePage = (user) => {
         }
     }, [user, _user])
 
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+
+    const times = [
+        "12:00 AM",
+        "12:15 AM",
+        "12:30 AM",
+        "12:45 AM",
+        "1:00 AM",
+        "1:15 AM",
+        "1:30 AM",
+        "1:45 AM",
+        "2:00 AM",
+        "2:15 AM",
+        "2:30 AM",
+        "2:45 AM",
+        "3:00 AM",
+        "3:15 AM",
+        "3:30 AM",
+        "3:45 AM",
+        "4:00 AM",
+        "4:15 AM",
+        "4:30 AM",
+        "4:45 AM", 
+        "5:00 AM",
+        "5:15 AM",
+        "5:30 AM",
+        "5:45 AM",
+        "6:00 AM",
+        "6:15 AM",
+        "6:30 AM",
+        "6:45 AM",
+        "7:00 AM",
+        "7:15 AM",
+        "7:30 AM",
+        "7:45 AM",
+        "8:00 AM",
+        "8:15 AM",
+        "8:30 AM",
+        "8:45 AM",
+        "9:00 AM",
+        "9:15 AM",
+        "9:30 AM",
+        "9:45 AM",
+        "10:00 AM",
+        "10:15 AM",
+        "10:30 AM",
+        "10:45 AM",
+        "11:00 AM",
+        "11:15 AM",
+        "11:30 AM",
+        "11:45 AM",
+        "12:00 PM",
+        "12:15 PM",
+        "12:30 PM",
+        "12:45 PM",
+        "1:00 PM",
+        "1:15 PM",
+        "1:30 PM",
+        "1:45 PM",
+        "2:00 PM",
+        "2:15 PM",
+        "2:30 PM",
+        "2:45 PM",
+        "3:00 PM",
+        "3:15 PM",
+        "3:30 PM",
+        "3:45 PM",
+        "4:00 PM",
+        "4:15 PM",
+        "4:30 PM",
+        "4:45 PM", 
+        "5:00 PM",
+        "5:15 PM",
+        "5:30 PM",
+        "5:45 PM",
+        "6:00 PM",
+        "6:15 PM",
+        "6:30 PM",
+        "6:45 PM",
+        "7:00 PM",
+        "7:15 PM",
+        "7:30 PM",
+        "7:45 PM",
+        "8:00 PM",
+        "8:15 PM",
+        "8:30 PM",
+        "8:45 PM",
+        "9:00 PM",
+        "9:15 PM",
+        "9:30 PM",
+        "9:45 PM",
+        "10:00 PM",
+        "10:15 PM",
+        "10:30 PM",
+        "10:45 PM",
+        "11:00 PM",
+        "11:15 PM",
+        "11:30 PM",
+        "11:45 PM",
+    ]
+
+    let myAvailability = [];
+
+    const handleChangeDay = (event) => {
+        setDay(event.target.value);
+    };
+
+    const handleChangeStartTime = (event) => {
+        setStartTime(event.target.value);
+    };
+
+    const handleChangeEndTime = (event) => {
+        setStartTime(event.target.value);
+    };
+
+    const addTime = () => {
+        console.log(day + ", " + startTime + "-" + endTime);
+        console.log(times.indexOf(startTime) + " vs " + times.indexOf(endTime));
+        myAvailability.push(new Item(day, startTime, endTime));
+    }
+/*
+    const showAvailability = () => {
+        if (myAvailability.length != 0){
+            let availcards = [];
+            foreach (avail in myAvailability){
+
+            }
+        }
+    }
+*/
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -138,9 +283,55 @@ const ProfilePage = (user) => {
                                 variant="contained"
                             >
                                 Update
-                  </Button>
+                            </Button>
                         </Box>
                     </Suspense>
+                </Box>
+                <Box
+                    display="flex" 
+                    alignItems="center"
+                    justifyContent="center" 
+                    sx={{mt: 2}}
+                >
+                    <Stack direction="row">
+                        <Autocomplete
+                            disablePortal
+                            id="DaySelector"
+                            value={day}
+                            onChange={(event, newValue) => {
+                                setDay(newValue);
+                            }}
+                            options={days}
+                            sx={{ width: {xs: 100, md: 300} }}
+                            renderInput={(params) => <TextField {...params} label="Day" />}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="StartTimeSelector"
+                            options={times}
+                            value={startTime}
+                            onChange={(event, newValue) => {setStartTime(newValue)}}
+                            sx={{ width: {xs: 100, md: 300} }}
+                            renderInput={(params) => <TextField {...params} label="Start Time" />}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="EndTimeSelector"
+                            value={endTime}
+                            onChange={(event, newValue) => {setEndTime(newValue)}}
+                            options={times}
+                            sx={{ width: {xs: 100, md: 300} }}
+                            renderInput={(params) => <TextField {...params} label="End Time" />}
+                        />
+                        <Button 
+                            aria-label='AddIcon' 
+                            variant="contained" 
+                            color="success"
+                            title="addbutton"
+                            onClick={addTime}>
+                            <AddIcon />
+                        </Button>
+                    </Stack>
                 </Box>
             </div>
         )
