f11c8f35b4ce85bf19a1465fcc9d068a9656228d
AlexisDougherty13 <alexisdougherty@ufl.edu>
2022-04-13 12:37:44 -0400

    Add break-apart for string to obj for preexisting routes

--- backend/src/database.go
+++ backend/src/database.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"strings"
+	"fmt"
 )
 
 var DB *gorm.DB
@@ -40,7 +41,30 @@ type Tutor struct {
 	//TODO: Courses via many2many join table?
 }
 
+type TimeSlot struct {
+	Day        string  `json:"day"`
+	StartTime  string  `json:"starttime"`
+	EndTime    string  `json:"endtime"`
+}
+
 func (tutor Tutor) MarshalJSON() ([]byte, error) {
+	foundAvailability := []TimeSlot{}
+	fmt.Println("We are all mad here")
+	fmt.Println(tutor.Availability)
+	days := strings.Split(tutor.Availability, ",")
+	fmt.Println(days)
+	for i := 0; i < len(days); i ++ {
+		
+		fmt.Println("|" + days[i] + "|")
+		parts := strings.Split(strings.TrimSpace(days[i]), " ")
+		fmt.Println(parts[0])
+		fmt.Println(parts[1] + parts[2])
+		fmt.Println(parts[3] + parts[4])
+		curr := TimeSlot{parts[0], parts[1] + " " + parts[2], parts[3] + " " + parts[4]}
+		foundAvailability = append(foundAvailability, curr)
+	}
+	fmt.Println("the dark arts better be worried oh boy!")
+	fmt.Println(foundAvailability)
 	return json.Marshal(&struct {
 		*User
 		Rating       float32  `json:"rating"`
