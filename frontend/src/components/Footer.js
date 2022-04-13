6bc3e819bb30af3eee3b6398c592c4a83a78a849
chris-brugal <chrisbrugal@yahoo.com>
2022-04-12 23:24:45 -0400

    refactor: fixed naming order to be alphabetical and linkedin icon to lower

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,22 +10,23 @@ const Footer = () => {
             <Typography
                 sx={{ padding: "0px", margin: "0px", border: "0px" }}
             >
-                <LinkedInIcon />
-                <Link href="https://www.linkedin.com/in/christopher-brugal" target="_blank" rel="noopener" underline="hover" color="white">
-                    Christopher Brugal
+                <LinkedInIcon sx={{ position: "relative", top: "5px" }} />
+                <Link href="https://www.linkedin.com/in/WillBAnders/" target="_blank" rel="noopener" underline="hover" color="white" >
+                    Blake Anderson
                 </Link>
                 {', '}
-                <Link href="https://www.linkedin.com/in/cole-kitroser-35646b170/" target="_blank" rel="noopener" underline="hover" color="white">
-                    Cole Kitroser
+                <Link href="https://www.linkedin.com/in/christopher-brugal" target="_blank" rel="noopener" underline="hover" color="white">
+                    Christopher Brugal
                 </Link>
                 {', '}
                 <Link href="https://www.linkedin.com/in/alexis-dougherty/" target="_blank" rel="noopener" underline="hover" color="white">
                     Alexis Dougherty
                 </Link>
                 {', '}
-                <Link href="https://www.linkedin.com/in/WillBAnders/" target="_blank" rel="noopener" underline="hover" color="white">
-                    Blake Anderson
+                <Link href="https://www.linkedin.com/in/cole-kitroser-35646b170/" target="_blank" rel="noopener" underline="hover" color="white">
+                    Cole Kitroser
                 </Link>
+
             </Typography>
             <Typography>
                 <br />Copyright © 2022 TutorsVILLE.
